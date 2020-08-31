50b7ff0bbfe18ae382d850963350203ef4dd15c7
Bonsen Wakjira <bonsenw@gmail.com>
2020-08-31 15:31:13 +1000

    Initialised Project Zombie_Practice_Game, This includes movement, shooting and importing sprites.

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float speed;
+    private Vector2 direction;
+
+    private Camera cam;
+    private Vector2 mousePos;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        cam = Camera.main;
+    }
+
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        direction = new Vector2(h, v);
+
+        Aiming();
+    }
+
+    void FixedUpdate()
+    {
+        MoveCharacter();
+    }
+
+    void MoveCharacter()
+    {
+        rb.MovePosition((Vector2)transform.position + (direction * speed * Time.deltaTime));
+    }
+
+    void Aiming()
+    {
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector2 lookDir = mousePos - rb.position;
+
+        float angle = (Mathf.Atan2(lookDir.y, lookDir.x)*Mathf.Rad2Deg);
+
+        rb.rotation = angle;
+    }
+}
---
+++ Assets/PlayerShoot.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    public Transform firePoint;
+    public LayerMask playerMask;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit2D hitInfo = Physics2D.Raycast(firePoint.position, firePoint.right, Mathf.Infinity, playerMask);
+
+        if (hitInfo)
+        {
+            Debug.Log(hitInfo.transform.name);
+        }
+    }
+}
