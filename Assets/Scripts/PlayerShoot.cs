4300b7b7b4441d665a5f0d62b747dc2c4d3dca92
Bonsen Wakjira <bonsenw@gmail.com>
2020-09-01 22:05:12 +1000

    Tried to implement bullet shoot particle effect manually, next time particles will be used or else, animation will be attached to gun instead of seperate.

--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -9,6 +9,8 @@ public class PlayerShoot : MonoBehaviour
 
     public LineRenderer rayLine;
     public GameObject impactFx;
+    public GameObject bulletFx;
+    public float angleOffset;
 
     void Update()
     {
@@ -23,11 +25,14 @@ public class PlayerShoot : MonoBehaviour
         RaycastHit2D hitInfo = Physics2D.Raycast(firePoint.position, firePoint.right, Mathf.Infinity, playerMask);
         int maxLength = 50;
 
+       // Instantiate(bulletFx, firePoint.position, firePoint.rotation * Quaternion.Euler(new Vector3(0, 0, angleOffset)));
+
         if (hitInfo)
         {
             //Enemy script (get component)
             //if enemy isnt null damage em
             Instantiate(impactFx, hitInfo.point, Quaternion.identity);
+            //Destroy impactFx
 
             rayLine.SetPosition(0, firePoint.position);
             rayLine.SetPosition(1, hitInfo.point);
